ce7810694cee97fef7ec4d7936a21d357ec0fe6d
RyeBread <rye@rye.moe>
2026-04-10 15:40:22 -0600

    Only export VecGraphStore from store

--- crates/vecgraph-store/src/lib.rs
+++ crates/vecgraph-store/src/lib.rs
@@ -3,9 +3,10 @@ mod node_ops;
 mod search_ops;
 mod store;
 
-pub use edge_ops::{delete_edge, get_edge, get_edges_for_node, get_vector, insert_edge};
-pub use node_ops::{
+use edge_ops::{delete_edge, get_edge, get_edges_for_node, get_vector, insert_edge};
+use node_ops::{
     delete_name_mapping, delete_node, get_name_mapping, get_node, insert_node, set_name_mapping,
 };
-pub use search_ops::search;
+use search_ops::search;
+
 pub use store::VecGraphStore;
