2583f279427f209ed8d1443f223752b9c5064f01
RyeBread <rye@rye.moe>
2026-04-22 09:19:43 -0600

    Add experimental_allow_proto3_optional option for github actions

--- crates/vecgraph-proto/build.rs
+++ crates/vecgraph-proto/build.rs
@@ -1,5 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_prost_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
         .build_server(true)
         .build_client(true)
         .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
