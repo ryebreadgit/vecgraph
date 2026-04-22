2a01896ec0af7289077067577a5ea900031467d2
RyeBread <rye@rye.moe>
2026-04-21 21:20:35 -0600

    Add gitt-cliff

--- devenv.nix
+++ devenv.nix
@@ -39,6 +39,28 @@
   scripts.run.exec = "cargo run";
   scripts.build.exec = "cargo build";
   scripts.build-release.exec = "cargo build --release";
+  scripts.changelog-gen.exec = ''
+    VERSION=$1
+    if [ -z "$VERSION" ]; then
+      echo "Usage: changelog-gen <version>"
+      exit 1
+    fi
+
+    git cliff --unreleased --tag "v$VERSION" --output /tmp/changelog-draft.md
+
+    ''${EDITOR:-nano} /tmp/changelog-draft.md
+
+    if [ -f CHANGELOG.md ]; then
+      printf "# Changelog\n\n" > /tmp/changelog-merged.md
+      grep -v "^# Changelog" /tmp/changelog-draft.md >> /tmp/changelog-merged.md
+      grep -v "^# Changelog" CHANGELOG.md >> /tmp/changelog-merged.md
+      mv /tmp/changelog-merged.md CHANGELOG.md
+    else
+      cp /tmp/changelog-draft.md CHANGELOG.md
+    fi
+
+    echo "CHANGELOG.md updated with new version $VERSION"
+  '';
 
   enterShell = ''
     echo "ðŸ¦€ Rust development environment loaded!"
